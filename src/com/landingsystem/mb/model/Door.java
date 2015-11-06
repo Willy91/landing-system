22fb6897abd8a5b989a7503d51c2a690640aacd4
Willy91 <williamarques91@gmail.com>
2015-11-06 14:15:33 +0100

    added right time

--- src/com/landingsystem/mb/model/Door.java
+++ src/com/landingsystem/mb/model/Door.java
@@ -3,6 +3,9 @@ package com.landingsystem.mb.model;
 public class Door extends Element {
 	public Door(){
 		super();
-		super.MAX_OUTGOING_TIME = 7000;
-		super.USUAL_OUTGOING_TIME = 150;	}
+		super.MAX_OUTGOING_TIME = 700;
+		super.USUAL_OUTGOING_TIME = 150;
+		super.MAX_RETRACTING_TIME = super.MAX_OUTGOING_TIME;
+		super.USUAL_RETRACTING_TIME = 160;
+	}
 }
--- src/com/landingsystem/mb/model/Gear.java
+++ src/com/landingsystem/mb/model/Gear.java
@@ -4,8 +4,10 @@ public class Gear extends Element  {
 
 	public Gear(){
 		super();
-		super.MAX_RETRACTING_TIME = 10000;
+		super.MAX_RETRACTING_TIME = 1000;
 		super.USUAL_RETRACTING_TIME = 280;
+		super.USUAL_OUTGOING_TIME = 240;
+		super.MAX_OUTGOING_TIME = 7000;
 	}
 
 }

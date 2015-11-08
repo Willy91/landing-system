f910353ee8448d14ef82bf2f72789455623f9593
Alexandre <bruneau@diveboard.com>
2015-11-08 17:52:23 +0100

    starting javadoc

--- src/com/landingsystem/mb/model/Element.java
+++ src/com/landingsystem/mb/model/Element.java
@@ -1,5 +1,10 @@
 package com.landingsystem.mb.model;
 
+/**
+ * 
+ * @author grosalex
+ *Element class that can represent an element of the system
+ */
 public abstract class Element {
 	private boolean status;
 	private boolean moving;
@@ -11,22 +16,40 @@ public abstract class Element {
 	protected int USUAL_OUTGOING_TIME;
 	protected int MAX_OUTGOING_TIME;
 	
+	/**
+	 * Default Constructor of Element Object
+	 */
 	public Element () {
 		this.moving = false;
 	}
 
+	/**
+	 * get current status of the element
+	 * @return true if the element is out and false if it's in
+	 */
 	public boolean isStatus() {
 		return status;
 	}
 
+	/**
+	 * set current status of the element
+	 * @param status set to true when out and false when in
+	 */
 	public void setStatus(boolean status) {
 		this.status = status;
 	}
 	
-
+	/**
+	 * is the element moving
+	 * @return true if the element is moving
+	 */
 	public boolean isMoving() {
 		return moving;
 	}
+	/**
+	 * Set if the element is moving
+	 * @param moving true if is moving and false if not
+	 */
 	public void setMoving(boolean moving) {
 		this.moving = moving;
 	}
--- src/com/landingsystem/mb/model/OutgoingThread.java
+++ src/com/landingsystem/mb/model/OutgoingThread.java
@@ -1,9 +1,16 @@
 package com.landingsystem.mb.model;
-
+/**
+ * This thread represent the action of putting something out
+ * @author grosalex
+ *
+ */
 public class OutgoingThread extends CentralThread {
 	public boolean done;
 	public Element el;
-	
+	/**
+	 * Default Constructor
+	 * @param in is the moving element
+	 */
 	public OutgoingThread(Element in){
 		super(in);
 		super.MAX_TIME = in.MAX_OUTGOING_TIME;
